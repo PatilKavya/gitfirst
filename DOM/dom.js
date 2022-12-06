91e34b7336be70471fb2a3a86434b5740cccab83
PatilKavya <kavyapatilgonalst@gmail.com>
2022-12-06 09:52:47 +0530

    add dom file

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,16 +1,16 @@
-//console.dir(document);
-//console.log(document.domain);
- //console.log(document.title);
-//onsole.log(document.body);
-// console.log(document.all);
-// var things=document.all;
-// for(let c of things){
-//     console.log(c);
-// }
+console.dir(document);
+console.log(document.domain);
+ console.log(document.title);
+onsole.log(document.body);
+console.log(document.all);
+var things=document.all;
+for(let c of things){
+    console.log(c);
+}
 
-// var header=document.getElementById("main-header");
-// header.style.borderBottom="solid 5px #000";
+var header=document.getElementById("main-header");
+header.style.borderBottom="solid 5px #000";
 
 var header=document.querySelector('.title');
-//header.style.fontWeight='bold';
+header.style.fontWeight='bold';
 header.style.color='green';
